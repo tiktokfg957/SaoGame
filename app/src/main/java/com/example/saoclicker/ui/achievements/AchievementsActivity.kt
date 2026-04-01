5daa0fab1de3a0b537bc1f58df6731ca7bb9f6c5
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-01 23:16:22 +0700

    Create AchievementsActivity.kt

---
+++ app/src/main/java/com/example/saoclicker/ui/achievements/AchievementsActivity.kt
@@ -0,0 +1,41 @@
+package com.example.saoclicker.ui.achievements
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.saoclicker.SAOClickerApplication
+import com.example.saoclicker.adapters.AchievementAdapter
+import com.example.saoclicker.databinding.ActivityAchievementsBinding
+
+class AchievementsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAchievementsBinding
+    private val viewModel: AchievementsViewModel by viewModels {
+        AchievementsViewModel.AchievementsViewModelFactory((application as SAOClickerApplication).repository)
+    }
+    private lateinit var adapter: AchievementAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAchievementsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.achievements)
+
+        adapter = AchievementAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.achievements.observe(this) { achievements ->
+            adapter.submitList(achievements)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
